bc87c1cbf8c69db0177c310106e2f1217d516f04
Praveen <praveen.mallela433@gmail.com>
2019-02-28 11:28:13 +0530

    commit message

---
+++ sample_src/sample_pack/test1.java
@@ -0,0 +1,23 @@
+package sample_pack;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.setProperty("webdriver.gecko.driver","D:\\MyDocs\\Selenium_Sample\\chromedriver.exe");
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability("marionette",true);
+		WebDriver driver= new FirefoxDriver(capabilities);
+		driver.get("https://www.google.com/");*/
+		System.setProperty("webdriver.chrome.driver", "D:\\MyDocs\\Selenium_Sample\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+}
